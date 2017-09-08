dfd566a77cde0bada12b479d9717bc7150a07712
义庆 石 <szsyq123@163.com>
2017-09-08 16:46:57 +0800

    9.8

---
+++ SportsStore.WebUI/Models/EFDbContext.cs
@@ -0,0 +1,24 @@
+namespace SportsStore.WebUI.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class EFDbContext : DbContext
+    {
+        public EFDbContext()
+            : base("name=EFDbContext")
+        {
+        }
+
+        public virtual DbSet<Products> Products { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Products>()
+                .Property(e => e.Price)
+                .HasPrecision(16, 2);
+        }
+    }
+}
